fc54489a654aa7c15c3e3a95a179af72fc48de5a
Sandeep Thomas <sandeepthomas@outlook.com>
2023-01-28 19:22:26 +0530

    update nx to 15.6.3

--- nx-react/apps/react/webpack.config.js
+++ nx-react/apps/react/webpack.config.js
@@ -1,13 +1,33 @@
-// @ts-check
-
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { composePlugins, withNx } = require('@nrwl/webpack');
+const { withReact } = require('@nrwl/react');
 const { withModuleFederation } = require('@nrwl/react/module-federation');
+
 const baseConfig = require('./module-federation.config');
 
-/**
- * @type {import('@nrwl/devkit').ModuleFederationConfig}
- **/
-const defaultConfig = {
+const config = {
   ...baseConfig,
 };
 
-module.exports = withModuleFederation(defaultConfig);
+const changeModuleFederationToScript = () => {
+  return (config, ctx) => {
+    for (let plugin of config.plugins) {
+      if (plugin instanceof ModuleFederationPlugin) {
+        plugin._options.library = {
+          type: 'var',
+          name: plugin._options.name,
+        };
+      }
+    }
+    config.output.scriptType = 'text/javascript';
+    return config;
+  };
+};
+
+// Nx plugins for webpack to build config object from Nx options and context.
+module.exports = composePlugins(
+  withNx(),
+  withReact(),
+  withModuleFederation(config),
+  changeModuleFederationToScript()
+);
